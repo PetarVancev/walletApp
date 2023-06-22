6bff3f9e525cc800406f945c5a113f3855852d73
PetarVancev <pvancev@gmail.com>
2023-06-22 16:12:04 +0200

    Changed db connection to remote db

--- db/db.ts
+++ db/db.ts
@@ -2,7 +2,8 @@ import pgPromise, { IDatabase, IMain } from "pg-promise";
 
 const pgp: IMain = pgPromise();
 
-const conn: string = "postgres://test:pass@localhost:5432/walletapp";
+const conn: string =
+  "postgresql://postgres:bv59hHUSxZ3gdwPpD4YH@containers-us-west-45.railway.app:7165/railway";
 const db: IDatabase<any> = pgp(conn);
 
 export default { pgp, db };
